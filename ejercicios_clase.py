249568fc4e3635fc53ac15592acd4faa11f87266
Nadia-1987 <63470922+Nadia-1987@users.noreply.github.com>
2020-06-23 20:31:34 -0300

    ejercicios clase funciones

--- ejercicios_clase.py
+++ ejercicios_clase.py
@@ -16,11 +16,16 @@ __email__ = "alumnos@inove.com.ar"
 __version__ = "1.1"
 
 import random
+import math
 
 
 def ej1():
     # Ejercicios con funciones del sistema
     numeros = [2, 4, 6, 8, 10, 12]
+
+    prom = promedio(numeros)
+
+    print('el promedio es',prom)
 
     '''
     Realice una funcion llamada "promedio" la cual
@@ -40,17 +45,57 @@ def ej1():
     Utilice esa función para calcular el promedio y luego
     imprima en pantalla el resultado
     '''
+    
+
+def promedio(numeros):
+        
+    if numeros == []:
+        print('lista vacia')
+    else:
+        promedio = sum(numeros) / len(numeros)
+           
+    return promedio
+
+def lista_aleatoria(inicio, fin, cantidad):
+    
+    lista = []
+
+    for i in range(cantidad):
+        lista.append(random.randint(inicio,fin))
+
+    return lista
+
 
 
 def ej2():
     # Ejercicios con modulos del sistema
     inicio = 0
     fin = 10
+    
+    lista_aleatoria2 = lista_aleatoria(0, 10, 5)
+    
+    print(lista_aleatoria2)
+
+      
+    numero_1 = random.choice (lista_aleatoria2)  
+    numero_2 = random.choice (lista_aleatoria2)
+
+    print ('los numeros aleatorios son:',numero_1,'y',numero_2) 
+
+    
+    raiz_cuadrada_1 = math.sqrt(numero_1)
+    raiz_cuadrada_2 = math.sqrt(numero_2)
+    
+    print('la raiz cuadrada de n1 es:',raiz_cuadrada_1,'y la del n2 es:',raiz_cuadrada_2)
 
     # Ejemplo de como obtener un numero aleatorio
     # entre inicio y fin
     # inicio <= numero <= fin
-    numero = random.randrange(inicio, fin+1)
+    
+     
+    
+
+
     # Documentación oficial de random
     # https://docs.python.org/3.7/library/random.html
 
@@ -64,6 +109,9 @@ def ej2():
     Dicha función debe retornar la lista de elementos random generados.
     '''
 
+
+    
+
     # numeros = lista_aleatoria (inicio, fin, cantidad)
 
     # Imprima en pantalla la lista de elementos generados
@@ -73,6 +121,10 @@ def ej2():
     # de la lista de elementos generados
     # numero_1 = random.choice(...)
     # numero_2 = random.choice(...)
+
+    
+      
+      
 
     # Importar en este programa/documento el modulo "math"
     # Calcular la raiz cuadrada (square root) de esos
@@ -87,13 +139,27 @@ def ej2():
     # raiz_cuadrada_2 = ....
 
 
+    
 def ej3():
-    # Ejercicios de listas y métodos
-    numeros = [2, 4, 6, 8, 10, 12]
+    numeros = [2, 6, 4, 7, 17, 12]
+
+    
+
+    lista_ordenada = ordenar(numeros)
+    print('la lista ordenada es',lista_ordenada)
+
+def ordenar(numeros):
+    numeros_ordenados = sorted(numeros)
+    return numeros_ordenados
+    
+    
+    
+
 
     '''
     Generar una una nueva funcion que se llame "ordenar",
     que utilizaremos para odernar la lista de numeros.
+
     Dentro de la función puede ordenar la lista
     usando bucles o las funciones nativas de Python
     La función no hace falta que retorne la lista ordenada,
@@ -102,6 +168,10 @@ def ej3():
     en la función afectan a la variable pasada como argumento)
 
     '''
+
+
+    
+
 
 
 def ej4():
@@ -120,7 +190,18 @@ def ej4():
     "count"
 
     '''
+    
+    lista_numeros = lista_aleatoria(0, 10, 5)
 
+    cantidad_uno = contar(lista_numeros, 1)
+    print('el numero 1 se repite:',cantidad_uno)
+
+def contar (numeros, numero):
+    repeticiones = numeros.count(numero)
+    
+    return repeticiones 
+     
+     
     # Por ejemplo creo una lista de 5 elemtnos
     # lista_numeros = lista_aleatoria(...,...,cantidad_numeros)
     # Luego quiero averiguar cuantas veces se repite el numero 3
@@ -128,7 +209,7 @@ def ej4():
 
 def ej5():
     # Ejercicios de listas y métodos
-    cantidad_numeros = 5
+ 
 
     '''
     Utilice la función "lista_aleatoria" para generar
@@ -144,6 +225,27 @@ def ej5():
     Recuerde que el elemento puede repetirse más de una
     vez en la lista.
     '''
+    lista_numeros = lista_aleatoria(1, 9, 5)
+    num_usuario = int(input('ingrese numero:\n'))
+    lista_posicion = buscar(lista_numeros, num_usuario)
+    print(lista_posicion)
+
+def buscar(numeros, numero):
+    lista = []
+    
+    
+    if numero in numeros:
+        posicion = numeros.index(numero)
+        lista.append(posicion)
+        
+          
+        
+    else:
+        print('lista vacia')
+        
+    return lista
+
+    #como generar una lista con varias posiciones de un numero que se repite.
 
     # Por ejemplo creo una lista de 5 elemtnos
     # lista_numeros = lista_aleatoria(...,...,cantidad_numeros)
@@ -153,8 +255,8 @@ def ej5():
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
-    ej1()
+    #ej1()
     #ej2()
     #ej3()
     #ej4()
-    #ej5()
+    ej5()
